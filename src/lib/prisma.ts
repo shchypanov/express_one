3f25e6155b058bf0d5dc511191cd7b57c25d006d
Vlad Shchypanov <shchipanovv@gmail.com>
2025-12-19 16:38:00 +0200

    add packages, expend requests.http

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,5 +1,12 @@
 import { PrismaClient } from "../../generated/prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
+import { Pool } from "pg";
 
-const prisma = new PrismaClient();
+const connectionString = process.env.DATABASE_URL;
+
+const pool = new Pool({ connectionString });
+const adapter = new PrismaPg(pool);
+
+const prisma = new PrismaClient({ adapter });
 
 export default prisma;
